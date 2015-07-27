5d0620538e2d5e53d43819fe133549ce72d68559
vadyalex <vadyalex@gmail.com>
2015-07-28 00:07:29 +0200

    Minor refactoring

--- src/main/java/me/vadyalex/petproject/service/configuration/Configuration.java
+++ src/main/java/me/vadyalex/petproject/service/configuration/Configuration.java
@@ -2,15 +2,37 @@ package me.vadyalex.petproject.service.configuration;
 
 
 import me.vadyalex.petproject.service.resource.MyResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.Spark;
 import spark.servlet.SparkApplication;
 
-public class Configuration implements SparkApplication {
+public class Configuration implements SparkApplication, AutoCloseable {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(Configuration.class);
 
     @Override
     public void init() {
 
-        Spark.get("/myresource", new MyResource());
+        final String PETPROJECT_ENV = System.getProperty("petproject.env");
 
+        if (PETPROJECT_ENV == null) {
+            LOGGER.info("PRODUCTION ENVIRONMENT");
+        } else if (PETPROJECT_ENV.equalsIgnoreCase("dev")) {
+            LOGGER.info("LOCAL DEVELOPMENT ENVIRONMENT");
+        } else if (PETPROJECT_ENV.equalsIgnoreCase("mock")) {
+            LOGGER.info("MOCKED INTEGRATION TEST");
+        }
+
+        Spark.get(
+                "/myresource",
+                new MyResource()
+        );
+
+    }
+
+    @Override
+    public void close() throws Exception {
+        LOGGER.info("RELEASING RESOURCES..");
     }
 }
