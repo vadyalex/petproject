92ae9788154fef2482a7003ad22535a156b33ab8
Vladyslav Aleksakhin <vadyalex@gmail.com>
2017-04-10 20:27:36 +0200

    Update ring middleware

--- src/petproject/handler.clj
+++ src/petproject/handler.clj
@@ -2,7 +2,7 @@
 
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [clojure.data.json :as json]
             [clojure.tools.logging :as log]))
 
@@ -38,4 +38,4 @@
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes api-defaults))
