54a304cf9e74400b99a8bd77f0ff6872919ea5c3
Vladyslav Aleksakhin <vadyalex@gmail.com>
2017-04-10 20:39:28 +0200

    Update handler tests

--- test/petproject/handler_test.clj
+++ test/petproject/handler_test.clj
@@ -5,11 +5,29 @@
             [petproject.handler :refer :all]))
 
 (deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
+  (testing "/myresource route"
+    (let [response (app (mock/request :get "/myresource"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (:body response) "{\"message\":\"Hello Word\"}"))))
 
   (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+    (let [response (app (mock/request :get "/unknown"))]
+      (is (= (:status response) 404))
+      (is (= (:body response) "Not Found"))))
+
+  (testing "/some/test route"
+    (let [response (app (mock/request :get "/some/test"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "THE ROOT /some/test"))))
+
+  (testing "/some/test/here route"
+    (let [response (app (mock/request :get "/some/test/here"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "THE /some/test/here"))))
+
+  (testing "/some/test/there route"
+    (let [response (app (mock/request :get "/some/test/there"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "THE /some/test/there"))))
+
+  )
